e3b0d6485f755fe0d8f60983346de58d2af1fbd0
cristopher.pereira <cristopher.pereira@cision.com>
2013-09-09 15:21:25 +0100

    Add item code added

---
+++ client/addNewsItem/addNewsItem.js
@@ -0,0 +1,38 @@
+Template.addNewNewsItemTemplate.events({
+  'click .add': function (event) {
+		Session.set("addingNewNewsItem",true);
+  }
+});
+
+Template.addNewNewsItemFormTemplate.events({
+  'click .save': function (event) {
+	var numVal = $(".num",event.target.parentNode.parentElement).val();
+	var imgVal = $(".img",event.target.parentNode.parentElement).val();
+
+	Blocks.insert({
+            
+            num: numVal,
+            img: imgVal,
+          });
+
+		Session.set("addingNewNewsItem",false);
+	},
+	  'click .cancel' : function (event) {
+  		Session.set("addingNewNewsItem",false);	
+  	}
+  });
+
+Template.addNewsItemContentPanel.helpers({
+  AddingNewNewsItem: function() {
+	    if(Session.get("addingNewNewsItem")==null){
+			Session.set("addingNewNewsItem",false);
+	    }
+	    if (!Session.get("addingNewNewsItem")){
+	    	return false;
+	    }
+	    else
+	    {
+	    	return true;
+	    }
+	}
+});

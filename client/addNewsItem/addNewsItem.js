41592ce841fc4de5bba5f9554b0b667c18029db7
luis.rente <luis.rente@cision.com>
2013-09-10 10:05:14 +0100

    Date added code added

--- client/addNewsItem/addNewsItem.js
+++ client/addNewsItem/addNewsItem.js
@@ -8,11 +8,16 @@ Template.addNewNewsItemFormTemplate.events({
   'click .save': function (event) {
 	var numVal = $(".num",event.target.parentNode.parentElement).val();
 	var imgVal = $(".img",event.target.parentNode.parentElement).val();
-
+	var date = new Date();
+	var day = date.getDate();
+	var month = date.getMonth()+1;
+	var year = date.getFullYear();
+	debugger;
 	Blocks.insert({
             
             num: numVal,
             img: imgVal,
+			dateAdded: month+"/"+day+"/"+year
           });
 
 		Session.set("addingNewNewsItem",false);
